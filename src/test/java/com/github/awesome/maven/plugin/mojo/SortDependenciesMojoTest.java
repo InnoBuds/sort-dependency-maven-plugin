c0f2deca873c6e012f25c87ba69104fd830058f1
codeboyzhou <imzhouchen@gmail.com>
2024-12-10 13:43:33 +0800

    test(main): Polish unit tests

--- src/test/java/com/github/awesome/maven/plugin/mojo/SortDependenciesMojoTest.java
+++ src/test/java/com/github/awesome/maven/plugin/mojo/SortDependenciesMojoTest.java
@@ -1,12 +1,21 @@
 package com.github.awesome.maven.plugin.mojo;
 
+import com.github.awesome.maven.plugin.util.XmlHelper;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class SortDependenciesMojoTest {
@@ -40,15 +49,33 @@ class SortDependenciesMojoTest {
     }
 
     @Test
-    void testExecute_DependenciesIncludeDependency() throws MojoExecutionException, NoSuchFieldException, IllegalAccessException {
+    void testExecute_DependenciesIncludeDependency() throws MojoExecutionException, NoSuchFieldException, IllegalAccessException, IOException {
         SortDependenciesMojo mojo = new SortDependenciesMojo();
         MavenProject project = mock(MavenProject.class);
-        when(project.getFile()).thenReturn(new File("src/test/resources/test-pom-dependencies-include-dependency.xml"));
+        File pomFile = new File("src/test/resources/test-pom-dependencies-include-dependency.xml");
+        when(project.getFile()).thenReturn(pomFile);
         when(project.getArtifactId()).thenReturn("test-pom-dependencies-include-dependency");
         Field projectField = SortDependenciesMojo.class.getDeclaredField("project");
         projectField.setAccessible(true);
         projectField.set(mojo, project);
         mojo.execute();
+        Document pomXmlDocument = XmlHelper.parse(pomFile);
+        Node dependencies = pomXmlDocument.getElementsByTagName("dependencies").item(0);
+        NodeList dependencyNodeList = dependencies.getChildNodes();
+        List<String> dependencyInfo = new ArrayList<>(dependencyNodeList.getLength());
+        for (int i = 0; i < dependencyNodeList.getLength(); i++) {
+            Node dependencyNode = dependencyNodeList.item(i);
+            if (dependencyNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element dependencyElement = (Element) dependencyNode;
+                final String groupId = dependencyElement.getElementsByTagName("groupId").item(0).getTextContent();
+                final String artifactId = dependencyElement.getElementsByTagName("artifactId").item(0).getTextContent();
+                final String version = dependencyElement.getElementsByTagName("version").item(0).getTextContent();
+                dependencyInfo.add(groupId + ":" + artifactId + ":" + version);
+            }
+        }
+        assertEquals("com.google:guava:33.3.1-jre", dependencyInfo.get(0));
+        assertEquals("org.apache.commons:commons-collections4:4.4", dependencyInfo.get(1));
+        assertEquals("org.apache.commons:commons-lang3:3.17.0", dependencyInfo.get(2));
         verify(project).getFile();
         verify(project).getArtifactId();
     }
